405caa3041acf18c8d62e70e87b598def324ff50
MooseWithBear <107022571+MooseWithBear@users.noreply.github.com>
2022-10-12 18:14:14 +0900

    [update] sub1_1 완성

--- mobile/js/common.js
+++ mobile/js/common.js
@@ -78,16 +78,16 @@ $('.topMove').hide();
     
 $(window).on('scroll',function(){ //스크롤 값의 변화가 생기면
     var scroll = $(window).scrollTop(); //스크롤의 거리
-    var scrollFoot = $('#footerArea').offset().top - $(window).height() + 60; // 푸터에서의 값 계산
+    var scrollFoot = $('.use').offset().top - $(window).height() + 60; // 푸터에서의 값 계산
     
     //$('.text').text(scroll);
     if(scroll > 300){ // 300이상의 거리가 발생되면
         $('.topMove').fadeIn('slow');  // top 보이기
 
         if(scroll < scrollFoot){ // footer보다 작으면 bottom:20, fixed
-            $('.topMove').css('bottom',55).css('position','fixed');
+            $('.topMove').css('bottom',25).css('position','fixed');
         } else { // footer보다 크면 bottom:200, absolute
-            $('.topMove').css('bottom',250).css('position','absolute');
+            $('.topMove').css('bottom',76).css('position','absolute');
         };
 
     }else{
