5f09e0cd7cd383035c7062a4f6cf467b9ae6dbde
MooseWithBear <107022571+MooseWithBear@users.noreply.github.com>
2022-10-16 16:45:33 +0900

    [edit] 일부 버그 수정

--- mobile/js/sub3.js
+++ mobile/js/sub3.js
@@ -1,29 +1,6 @@
 $(document).ready(function () {
 
-
-// //! sub2_2
-// //* FAQ
-// $(".contentArea dl").each(function (index) {
-//   $(this).find("a").click(function (e){
-//     e.preventDefault();
-//     if ($(this).parent().parent().hasClass("on")) { //dl이 on 갖고있으면
-//         $(this).parent().parent().removeClass("on").find('dd').slideUp("fast");
-//         $(this).find("i").removeClass("fa-chevron-up").addClass("fa-chevron-down");
-//     } else {
-//         $('.contentArea dd').slideUp("fast").parent().removeClass("on");
-
-//           $(".contentArea i").removeClass("fa-chevron-up").addClass("fa-chevron-down");
-//           $(".contentArea dl:eq(" + index + ") dd")
-//             .slideDown("slow")
-//             .parent()
-//             .addClass("on");
-//           $(this).find("i").removeClass("fa-chevron-down").addClass("fa-chevron-up");
-
-//     }   
-//   })
-// })
-
-//
+$('.overview').find('a').html('<i class="fa-solid fa-magnifying-glass-plus"></i>')
 
 //! 중단 공통 클릭
 $('.overview li').each(function (index){
--- mobile/js/sub4.js
+++ mobile/js/sub4.js
@@ -1,7 +1,20 @@
+//! sub 4_1 미디어센터
 $(document).ready(function () {
 
+
+
+
+
+
+
+
+
+
+
+// ! sub4_2 FAQ
+
   var faqArticle = $(".sub_faq ul li"); // 전체 li
-  $('.sub_faq').find('a').append('<i class="fa-regular fa-square-plus"></i>')
+  $(".sub_faq").find("a").append('<i class="fa-regular fa-square-plus"></i>');
   $(".sub_faq ul li p a").click(function (e) {
     e.preventDefault();
     var answer = $(this).parent().parent(); // 해당 li
@@ -9,35 +22,36 @@ $(document).ready(function () {
       // li에 on이 있으면
       answer.removeClass("on"); // on없애고
       answer.children().next().stop().slideUp("fast"); // 본인 닫아라
-      $(this).find('i').removeClass('fa-square-minus').addClass('fa-square-plus')
+      $(this).find("i").removeClass("fa-square-minus").addClass("fa-square-plus");
     } else {
       // li에 on이 없으면
       faqArticle.removeClass("on"); // 다른 li에 on 없애고
-      $('.sub_faq ul li p').find('i').removeClass('fa-square-minus').addClass('fa-square-plus')
+      $(".sub_faq ul li p").find("i").removeClass("fa-square-minus").addClass("fa-square-plus");
 
       answer.addClass("on"); // 내 li에만 on을 넣어라
       faqArticle.children().next().stop().slideUp("fast");
       answer.children().next().stop().slideDown("fast");
-      $(this).find('i').removeClass('fa-square-plus').addClass('fa-square-minus')
-
+      $(this).find("i").removeClass("fa-square-plus").addClass("fa-square-minus");
     }
   });
 
   $(".all").toggle(
     function (e) {
       e.preventDefault();
-
-      $(this).html('답변 모두 닫기 <i class="fa-regular fa-square-minus"></i>')
-      faqArticle.find('i').removeClass('fa-square-plus').addClass('fa-square-minus')
+      $(this).html('답변 모두 닫기 <i class="fa-regular fa-square-minus"></i>');
+      faqArticle.find("i").removeClass("fa-square-plus").addClass("fa-square-minus");
       faqArticle.children().next().stop().slideDown("fast");
     },
     function (e) {
       e.preventDefault();
-
-      $(this).html('답변 모두 열기 <i class="fa-regular fa-square-plus"></i>')
-      faqArticle.find('i').removeClass('fa-square-minus').addClass('fa-square-plus')
+      $(this).html('답변 모두 열기 <i class="fa-regular fa-square-plus"></i>');
+      faqArticle.find("i").removeClass("fa-square-minus").addClass("fa-square-plus");
       faqArticle.children().next().stop().slideUp("fast");
-
     }
   );
+
+
+  //
+
 });
+
