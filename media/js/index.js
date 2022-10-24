a1fb2e92113366500678761dc6d0ef6502f567ea
MooseWithBear <107022571+MooseWithBear@users.noreply.github.com>
2022-10-25 00:27:01 +0900

    [update] hover 이미지 추가

--- media/js/index.js
+++ media/js/index.js
@@ -1,8 +1,55 @@
-$(document).ready(function() {
+// $(document).ready(function () {
+// //   $(".about li").each(function (e) {
+// //     $(".about li:eq("+e+")").css('background', 'url("./images/about_'+ (e+1) +'.webp")')
+// //   });
 
+// $('.ab_01 li').each(function (index){
 
+//     $(this).hover(function (){
+//     // console.log(index)
 
+//     $(".ab_01 li:eq("+index+")").css('background','url("./images/about_'+ (index+1) +'p.webp") 0 0 no-repeat').css('background-size','contain')
+//     }, function(){
+//         $(".ab_01 li:eq("+index+")").css('background','url("./images/about_'+ (index+1) +'.webp") 0 0 no-repeat').css('background-size','contain')
+//     })
+// })
 
-
+// $('.ab_02 li').each(function (index){
     
-})
+//     $(this).hover(function (){
+//     // console.log(index)
+
+//     $(".ab_02 li:eq("+index+")").css('background','url("./images/about2_'+ (index+1) +'p.webp") 50% 50% no-repeat').css('background-size','cover')
+//     }, function(){
+//         $(".ab_02 li:eq("+index+")").css('background','url("./images/about2_'+ (index+1) +'.webp") 50% 50% no-repeat').css('background-size','cover')
+//     })
+// })
+
+// // $(".ab_01 li").click(function(){
+// //     var num = $("li").index(this);
+// //     console.log(num)
+// //     (".ab_01 li:eq("+num+")").css('background','url("./images/about_'+ (num+1) +'p.webp") 0 0 no-repeat').css('background-size','contain')
+
+
+
+
+// // }
+
+// // )
+
+
+
+// //   $(".cast li").each(function (e) {
+// //     $(".cast li:eq("+e+")").css('background', 'url("./images/about_'+ (e+1) +'.webp") 50% 50% no-repeat').css('background-size', 'contain')
+
+// //   });
+
+// //   $(".trail_01 div").css();
+// //   $(".trail_02 li").each(function (e) {
+
+// //   });
+
+// //   $(".gallery li").each(function (e) {
+
+// //   });
+// });
