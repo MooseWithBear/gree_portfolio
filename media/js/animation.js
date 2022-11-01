9cd0b0201d024de6e10cb0ee0f25a5ac11d2afb1
MooseWithBear <107022571+MooseWithBear@users.noreply.github.com>
2022-11-01 09:47:43 +0900

    [update] sub1 animation temp

---
+++ media/js/animation.js
@@ -0,0 +1,33 @@
+$('.c-supermario div').each(function(index){
+    
+    var nan = (index*(Math.random()*15)-10 * 0.13)
+    var nandiv = Math.floor(Math.random()*100)
+    var nandiv2 = Math.floor(Math.random()*250)
+    var nandiv3 = Math.floor(Math.random()*300)
+    var nandiv4 = Math.floor(Math.random()*300)
+
+
+    $('.c-supermario div:eq('+nandiv+')').css({
+        'transform': 'translate('+(-nan)+'px,'+nan+'px)',
+        'transition':'all 1s ease-out'
+        
+    })
+    $('.c-supermario div:eq('+nandiv2+')').css({
+        'transform': 'translate('+(nan)+'px,'+nan+'px)',
+        'transition':'all 1s ease-out'
+        
+    })
+    $('.c-supermario div:eq('+nandiv3+')').css({
+        'transform': 'translate('+(-nan)+'px,'+-nan+'px)',
+        'transition':'all 1s ease-out'
+        
+    })
+    $('.c-supermario div:eq('+nandiv4+')').css({
+        'transform': 'translate('+(+nan)+'px,'+-nan+'px)',
+        'transition':'all 1s ease-out'
+        
+    })
+    
+})
+
+
