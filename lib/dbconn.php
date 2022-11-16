36a2f4f88f9af51857046e3277aa192de8e1e33c
KIM SHINSEONG <moosewithbear@gmail.com>
2022-11-16 13:19:10 +0900

    [update] 서버보안

--- lib/dbconn.php
+++ lib/dbconn.php
@@ -1,6 +1,9 @@
+
+
+
 <?
-    $connect=mysql_connect( "localhost", "moosewithbear", "moose001") or  
+    $connect=mysql_connect( "localhost", "ssk", "1234") or  
         die( "SQL server에 연결할 수 없습니다."); 
 
-    mysql_select_db("moosewithbear",$connect);
+    mysql_select_db("ssk_db",$connect);
 ?>
